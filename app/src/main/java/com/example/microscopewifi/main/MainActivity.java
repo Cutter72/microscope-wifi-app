9d328a86ab04e5b0930aad99d7c725e8869857e8
Paweł Drelich <drelich_pawel@o2.pl>
2020-11-19 19:21:23 +0100

    Go to med plus btn created.

--- app/src/main/java/com/example/microscopewifi/main/MainActivity.java
+++ app/src/main/java/com/example/microscopewifi/main/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.microscopewifi.main;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.microscopewifi.R;
 
@@ -12,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickGoToMedPlus(View view) {
+        //go to med plus
     }
 }
